f42775b38743099faf808e8486bd20dbf7a54bae
Brad Ison <brad.ison@redhat.com>
2018-10-29 15:20:20 -0400

    Add new ClusterAutoscaler types

--- pkg/apis/autoscaling/v1alpha1/doc.go
+++ pkg/apis/autoscaling/v1alpha1/doc.go
@@ -1,3 +1,4 @@
-// +k8s:deepcopy-gen=package
+// Package v1alpha1 contains API Schema definitions for the autoscaling v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
 // +groupName=autoscaling.openshift.io
 package v1alpha1
