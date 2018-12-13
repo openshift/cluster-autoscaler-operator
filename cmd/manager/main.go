ac18d85dddc673e3d481bc7430903f823e7cc54e
Brad Ison <brad.ison@redhat.com>
2018-12-13 12:34:14 -0500

    Remove unused operator-sdk dependency

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -7,14 +7,12 @@ import (
 	"github.com/golang/glog"
 	"github.com/openshift/cluster-autoscaler-operator/pkg/operator"
 	"github.com/openshift/cluster-autoscaler-operator/version"
-	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 func printVersion() {
 	glog.Infof("Go Version: %s", runtime.Version())
 	glog.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
-	glog.Infof("operator-sdk Version: %v", sdkVersion.Version)
 	glog.Infof("cluster-autoscaler-operator Version: %v", version.Version)
 }
 
