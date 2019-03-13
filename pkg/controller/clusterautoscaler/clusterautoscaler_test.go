e86fd86a53af2c93e63bca229423cae34ecb1d08
Michael Gugino <mgugino@redhat.com>
2019-03-13 11:49:13 -0400

    ca_controller: implement unit tests
    Add unit tests for AvailableAndUpdated() to ensure
    cao status=progressing/available logic is correct.

--- pkg/controller/clusterautoscaler/clusterautoscaler_test.go
+++ pkg/controller/clusterautoscaler/clusterautoscaler_test.go
@@ -2,11 +2,16 @@ package clusterautoscaler
 
 import (
 	"fmt"
+	"github.com/openshift/cluster-autoscaler-operator/pkg/apis"
+	autoscalingv1alpha1 "github.com/openshift/cluster-autoscaler-operator/pkg/apis/autoscaling/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"strings"
 	"testing"
-
-	autoscalingv1alpha1 "github.com/openshift/cluster-autoscaler-operator/pkg/apis/autoscaling/v1alpha1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -28,6 +33,10 @@ var (
 	NvidiaGPUMin           int32 = 4
 	NvidiaGPUMax           int32 = 8
 )
+
+func init() {
+	apis.AddToScheme(scheme.Scheme)
+}
 
 func NewClusterAutoscaler() *autoscalingv1alpha1.ClusterAutoscaler {
 	// TODO: Maybe just deserialize this from a YAML file?
@@ -122,4 +131,120 @@ func TestAutoscalerArgs(t *testing.T) {
 			t.Fatalf("found arg expected to be missing: %s", e)
 		}
 	}
+}
+
+// This test ensures we can actually get an autoscaler with fakeclient/client.
+// fakeclient.NewFakeClientWithScheme will os.Exit(1) with invalid scheme.
+func TestCanGetca(t *testing.T) {
+	_ = fakeclient.NewFakeClient(NewClusterAutoscaler())
+}
+
+// newFakeReconciler returns a new reconcile.Reconciler with a fake client
+func newFakeReconciler(initObjects ...runtime.Object) *Reconciler {
+	fakeClient := fakeclient.NewFakeClient(initObjects...)
+	return &Reconciler{
+		client: fakeClient,
+		scheme: scheme.Scheme,
+	}
+}
+
+func TestAvailableAndUpdated(t *testing.T) {
+	ca := NewClusterAutoscaler()
+	dep1 := appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cluster-autoscaler-test",
+			Namespace: TestNamespace,
+			Annotations: map[string]string{
+				"release.openshift.io/version": "test-1",
+			},
+			Generation: 1,
+		},
+		Status: appsv1.DeploymentStatus{
+			ObservedGeneration: 1,
+			UpdatedReplicas:    1,
+			Replicas:           1,
+			AvailableReplicas:  1,
+		},
+	}
+	dep2 := appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cluster-autoscaler-test",
+			Namespace: TestNamespace,
+			Annotations: map[string]string{
+				"release.openshift.io/version": "test-2",
+			},
+			Generation: 1,
+		},
+		Status: appsv1.DeploymentStatus{
+			ObservedGeneration: 1,
+			UpdatedReplicas:    1,
+			Replicas:           1,
+			AvailableReplicas:  1,
+		},
+	}
+	cfg1 := Config{
+		ReleaseVersion: "test-1",
+		Name:           "test",
+		Namespace:      TestNamespace,
+	}
+	cfg2 := Config{
+		ReleaseVersion: "test-2",
+		Name:           "test",
+		Namespace:      TestNamespace,
+	}
+	cfg3 := Config{
+		ReleaseVersion: "test-2",
+		Name:           "test2",
+		Namespace:      TestNamespace,
+	}
+	tCases := []struct {
+		expectedError error
+		expectedOk    bool
+		c             *Config
+		d             *appsv1.Deployment
+	}{
+		// Case 0: should pass, returns true, nil.
+		{
+			expectedError: nil,
+			expectedOk:    true,
+			c:             &cfg1,
+			d:             &dep1,
+		},
+		// Case 1: CA found is wrong version, should return false, nil.
+		{
+			expectedError: nil,
+			expectedOk:    false,
+			c:             &cfg2,
+			d:             &dep1,
+		},
+		// Case 2: No CA in namespace, returns true, nil.
+		{
+			expectedError: nil,
+			expectedOk:    true,
+			c:             &cfg3,
+			d:             &dep1,
+		},
+		// Case 3: No deployment found, returns false, nil.
+		{
+			expectedError: nil,
+			expectedOk:    false,
+			c:             &cfg1,
+			d:             &appsv1.Deployment{},
+		},
+		// Case 4: Deployment wrong annotation, returns false, nil.
+		{
+			expectedError: nil,
+			expectedOk:    false,
+			c:             &cfg1,
+			d:             &dep2,
+		},
+	}
+	for i, tc := range tCases {
+		r := newFakeReconciler(ca, tc.d)
+		r.SetConfig(tc.c)
+		ok, err := r.AvailableAndUpdated()
+		assert.Equal(t, tc.expectedOk, ok, "case %v: expected true", i)
+		assert.Equal(t, tc.expectedError, err, "case %v: expected nil", i)
+	}
+
 }
