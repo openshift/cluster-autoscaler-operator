a3dc3a4fbb10306db5e9f5cb0761f47e6fd8a3d6
Brad Ison <brad.ison@redhat.com>
2019-04-04 16:12:12 +0200

    cmd/manager: Simplify klog initialization

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -16,20 +16,10 @@ func printVersion() {
 }
 
 func main() {
+	klog.InitFlags(nil)
+	flag.Set("logtostderr", "true")
 	flag.Set("alsologtostderr", "true")
 	flag.Parse()
-
-	klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
-	klog.InitFlags(klogFlags)
-
-	// Sync the glog and klog flags.
-	flag.CommandLine.VisitAll(func(f1 *flag.Flag) {
-		f2 := klogFlags.Lookup(f1.Name)
-		if f2 != nil {
-			value := f1.Value.String()
-			f2.Value.Set(value)
-		}
-	})
 
 	printVersion()
 
