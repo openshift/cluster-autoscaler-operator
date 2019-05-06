6d42815743304f773e37c461c24d35c9125db401
Jan Chaloupka <jchaloup@redhat.com>
2019-05-06 13:10:18 +0200

    Describe more autoscaler resource fields

--- pkg/apis/autoscaling/v1/clusterautoscaler_types.go
+++ pkg/apis/autoscaling/v1/clusterautoscaler_types.go
@@ -10,10 +10,19 @@ func init() {
 
 // ClusterAutoscalerSpec defines the desired state of ClusterAutoscaler
 type ClusterAutoscalerSpec struct {
-	ResourceLimits       *ResourceLimits  `json:"resourceLimits,omitempty"`
-	ScaleDown            *ScaleDownConfig `json:"scaleDown,omitempty"`
-	MaxPodGracePeriod    *int32           `json:"maxPodGracePeriod,omitempty"`
-	PodPriorityThreshold *int32           `json:"podPriorityThreshold,omitempty"`
+	// Constraints of autoscaling resources
+	ResourceLimits *ResourceLimits `json:"resourceLimits,omitempty"`
+
+	// Configuration of scale down operation
+	ScaleDown *ScaleDownConfig `json:"scaleDown,omitempty"`
+
+	// Gives pods graceful termination time before scaling down
+	MaxPodGracePeriod *int32 `json:"maxPodGracePeriod,omitempty"`
+
+	// To allow users to schedule "best-effort" pods, which shouldn't trigger
+	// Cluster Autoscaler actions, but only run when there are spare resources available,
+	// More info: https://github.com/kubernetes/autoscaler/blob/master/cluster-autoscaler/FAQ.md#how-does-cluster-autoscaler-work-with-pod-priority-and-preemption
+	PodPriorityThreshold *int32 `json:"podPriorityThreshold,omitempty"`
 }
 
 // ClusterAutoscalerStatus defines the observed state of ClusterAutoscaler
@@ -26,11 +35,15 @@ type ClusterAutoscalerStatus struct {
 // ClusterAutoscaler is the Schema for the clusterautoscalers API
 // +k8s:openapi-gen=true
 // +kubebuilder:subresource:status
+// +genclient:nonNamespaced
 type ClusterAutoscaler struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   ClusterAutoscalerSpec   `json:"spec,omitempty"`
+	// Desired state of ClusterAutoscaler resource
+	Spec ClusterAutoscalerSpec `json:"spec,omitempty"`
+
+	// Most recently observed status of ClusterAutoscaler resource
 	Status ClusterAutoscalerStatus `json:"status,omitempty"`
 }
 
@@ -44,11 +57,22 @@ type ClusterAutoscalerList struct {
 }
 
 type ResourceLimits struct {
+	// Maximum number of nodes in all node groups.
+	// Cluster autoscaler will not grow the cluster beyond this number.
 	// +kubebuilder:validation:Minimum=0
-	MaxNodesTotal *int32         `json:"maxNodesTotal,omitempty"`
-	Cores         *ResourceRange `json:"cores,omitempty"`
-	Memory        *ResourceRange `json:"memory,omitempty"`
-	GPUS          []GPULimit     `json:"gpus,omitempty"`
+	MaxNodesTotal *int32 `json:"maxNodesTotal,omitempty"`
+
+	// Minimum and maximum number of cores in cluster, in the format <min>:<max>.
+	// Cluster autoscaler will not scale the cluster beyond these numbers.
+	Cores *ResourceRange `json:"cores,omitempty"`
+
+	// Minimum and maximum number of gigabytes of memory in cluster, in the format <min>:<max>.
+	// Cluster autoscaler will not scale the cluster beyond these numbers.
+	Memory *ResourceRange `json:"memory,omitempty"`
+
+	// Minimum and maximum number of different GPUs in cluster, in the format <gpu_type>:<min>:<max>.
+	// Cluster autoscaler will not scale the cluster beyond these numbers. Can be passed multiple times.
+	GPUS []GPULimit `json:"gpus,omitempty"`
 }
 
 type GPULimit struct {
@@ -68,13 +92,22 @@ type ResourceRange struct {
 }
 
 type ScaleDownConfig struct {
+	// Should CA scale down the cluster
 	Enabled bool `json:"enabled"`
+
+	// How long after scale up that scale down evaluation resumes
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	DelayAfterAdd *string `json:"delayAfterAdd,omitempty"`
+
+	// How long after node deletion that scale down evaluation resumes, defaults to scan-interval
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	DelayAfterDelete *string `json:"delayAfterDelete,omitempty"`
+
+	// How long after scale down failure that scale down evaluation resumes
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	DelayAfterFailure *string `json:"delayAfterFailure,omitempty"`
+
+	// How long a node should be unneeded before it is eligible for scale down
 	// +kubebuilder:validation:Pattern=([0-9]*(\.[0-9]*)?[a-z]+)+
 	UnneededTime *string `json:"unneededTime,omitempty"`
 }
